20bbb95f2321d8c546ea2d9a5acbe61c7533b551
JuanRMadrid <juan.jrmadridm@hotmail.com>
2024-04-10 01:59:11 -0600

    avanzando2

--- src/controllers/con_postulante.js
+++ src/controllers/con_postulante.js
@@ -15,7 +15,7 @@ const createusrPostu = async (req, res) => { // Crear un nuevo usuario para post
     console.error('Error trying adding new data:', error);
   }
 }
-const login = (req, res) => {
+const login = (req, res) => { //Login de usuario
   try {
     const { nickname, password } = req.body;
     comprobarUsuario(nickname, password, (error, existe) => {
@@ -23,7 +23,7 @@ const login = (req, res) => {
         console.error("Failure to verify user", error);
       }
       if (existe) {
-        res.json({ message: "Funciono" }) // cambiar cuando se cree la ruta de vacante
+        res.redirect('/postulante');
       }
       else {
         res.redirect('/postulante/reg_postulante.html');
@@ -36,6 +36,31 @@ const login = (req, res) => {
     res.send(error.message);
   }
 }
+const createPostulante = async (req, res) => { //Crear Postulante
+  try {
+    const { NumeroIdentidad, Nombres, Apellidos, CorreoElectronico, telefono, FechaNacimiento, Sexo, Direccion, Estadop, Area, Descripcion, Nickname } = req.body;
+    const data = { NumeroIdentidad, Nombres, Apellidos, CorreoElectronico, telefono, FechaNacimiento, Sexo, Direccion, Estadop, Area, Descripcion, Nickname }
+    const connection = await getConnection();
+    await connection.query('INSERT INTO postulante set ?', data);
+    res.redirect('/postulante/postulante');
+  }
+  catch (error) {
+    res.status(500);
+    console.error('Error trying adding new data:', error);
+  }
+}
+const createContrato = async (req, res) => { //crear Contrato
+  try {
+    const { ContratoID, VacanteID, NumeroIdentidad, TipoContrato, FechaInicio, FechaVencimiento, Horario, Sueldo, DiasLaborales, DiasVacaciones, Departamento } = req.body;
+    const data = { ContratoID, VacanteID, NumeroIdentidad, TipoContrato, FechaInicio, FechaVencimiento, Horario, Sueldo, DiasLaborales, DiasVacaciones, Departamento }
+    const connection = await getConnection();
+    await connection.query('INSERT INTO contrato set ?',data);
+    res.redirect('/postulante');
+  } catch (error) {
+    res.status(500);
+    console.error('Error trying adding new data:', error);
+  }
+}
 /* GET / READ / LEER / SELECT */
 const getusrPostu = async (req, res) => { // Leer los usuarios de los postulantes
   try {
@@ -43,7 +68,31 @@ const getusrPostu = async (req, res) => { // Leer los usuarios de los postulante
     const result = await connection.query("SELECT * FROM usr_postu");
     res.render('postulante/usr_postu.ejs', { resQuery: result });
   } catch (error) {
-
+    console.error('Error:', error);
+    res.status(500);
+    res.send(error.message);
+  }
+}
+const getPostulante = async (req, res) => { //Leer los postulantes
+  try {
+    const connection = await getConnection();
+    const result = await connection.query("Select * from postulante");
+    res.render('postulante/postulante.ejs', { resQuery: result })
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500);
+    res.send(error.message);
+  }
+}
+const getVacantes = async (req, res) => { //Leer las vacantes formadas por las empresas
+  try {
+    const connection = await getConnection();
+    const result = await connection.query("Select * from vacante");
+    res.render('postulante/vacantes.ejs', { resQuery: result })
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500);
+    res.send(error.message);
   }
 }
 /* PUT / UPDATE / ACTUALIZAR / UPDATE */
@@ -52,7 +101,7 @@ const getusrPostu = async (req, res) => { // Leer los usuarios de los postulante
 
 // Exportar el controlador para que pueda ser utilizado en otros archivos
 export const methods = {
-  getusrPostu,
+  getusrPostu, getPostulante, getVacantes,
   login,
-  createusrPostu
+  createusrPostu, createPostulante,createContrato
 }
--- src/routes/rou_postu.js
+++ src/routes/rou_postu.js
@@ -5,15 +5,15 @@ import bodyParser from 'body-parser';
 routerPostu.use(bodyParser.urlencoded({ extended: true }));
 
 /* POST / CREATE / CREAR / INSERT */
-routerPostu.post('/usuario', PostulanteControllers.createusrPostu); // crear usuario
+routerPostu.post('/usuario', PostulanteControllers.createusrPostu); //crear usuario
 routerPostu.post('/login',PostulanteControllers.login); //login usuario
-// routerPostu.post('/postulante/postulante',usrPostuController.createPostulante); //crear postulante
-// routerPostu.post('/postulante/contrato',usrPostuController.createContrato); //crear contrato
+routerPostu.post('/postulante',PostulanteControllers.createPostulante); //Crear Postulante
+routerPostu.post('/contrato',PostulanteControllers.createContrato); //Crear contrato
 
 /* GET / READ / LEER / SELECT */
-routerPostu.get('/usuario',PostulanteControllers.getusrPostu);
-// routerPostu.get('/postulante/postulante',usrPostuController.getPostulante);
-// routerPostu.get('/postulante',usrPostuController.getVacantes);
+routerPostu.get('/usuario',PostulanteControllers.getusrPostu); //Leer los usuarios de los postulantes
+routerPostu.get('/postulante',PostulanteControllers.getPostulante); //Leer los postulantes
+routerPostu.get('/',PostulanteControllers.getVacantes); //Leer las vacantes formadas por las empresas
 
 /* PUT / UPDATE / ACTUALIZAR / UPDATE */
 /* DELETE / DELETE / BORRAR / DELETE */
